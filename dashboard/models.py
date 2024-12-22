2c5d2597e858196d200cec75db6a11d5aa539f83
Jason Javier <jasonjjavier@gmail.com>
2024-12-21 22:15:34 -0400

    added ReceiptModel

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ReceiptModel(models.Model):
+    company = models.CharField(max_length=250)
+    product = models.CharField(max_length=250)
+    price = models.FloatField(max_length=100)
+    quantity = models.IntegerField()
+    hst = models.FloatField(max_length=2)
+    date_added = models.DateTimeField(auto_now=True)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
 
-# Create your views here.
+# class IndexView(generic.View):
+#
